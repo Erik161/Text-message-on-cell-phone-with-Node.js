66f191bdc713800a62a96fbd33031bcb12e3e5a3
Erik Hernandez <erikestuardo_161@hotmail.com>
2019-05-01 12:46:12 -0600

    add modified index and download dotenv

---
+++ index/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const accountSid =  proces.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+    to: process.env.MY_PHONE_NUMBER,
+    from: '+17743146925',
+    body: 'Hello World'
+})
+    .then(messages => console.log(messages.sid));
+     
