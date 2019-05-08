7e558cb4adac4ae21eeb356797b90bf37287a429
Erik Hernandez <erikestuardo_161@hotmail.com>
2019-05-07 20:31:20 -0600

    .

--- index/index.js
+++ index/index.js
@@ -5,11 +5,10 @@ const authToken = process.env.AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
+
 client.messages.create({
     to: process.env.MY_PHONE_NUMBER,
     from: '+17743146925',
     body: 'Hello World'
 })
     .then(messages => console.log(messages.sid));
-
-
